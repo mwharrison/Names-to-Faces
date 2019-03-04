b04d14b0948a3171c248ccd254134cb23b05f766
Michael Harrison <mwharrison@rice.edu>
2019-03-04 14:45:32 -0600

    conform to codable protocol

--- Names to Faces/ViewController.swift
+++ Names to Faces/ViewController.swift
@@ -17,8 +17,12 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         let defaults = UserDefaults.standard
         
         if let savedPeople = defaults.object(forKey: "people") as? Data {
-            if let decodedPeople = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedPeople) as? [Person] {
-                people = decodedPeople ?? [Person]()
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("Failed to load people.")
             }
             
         }
@@ -48,6 +52,8 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         collectionView?.reloadData()
         
         dismiss(animated: true)
+        
+        self.save()
     }
     
     func getDocumentsDirectory() -> URL {
@@ -97,9 +103,12 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     }
     
     func save() {
-        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: people, requiringSecureCoding: false) {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(people) {
             let defaults = UserDefaults.standard
             defaults.set(savedData, forKey: "people")
+        } else {
+            print("Failed to save people.")
         }
     }
 
