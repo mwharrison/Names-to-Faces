0e42efbc53e51446c44c61180b7f0fc367f765d7
Michael Harrison <mwharrison@rice.edu>
2019-03-04 11:31:31 -0600

    initial commit

---
+++ Names to Faces/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Names to Faces
+//
+//  Created by Michael Harrison on 3/4/19.
+//  Copyright © 2019 Myco Labs LLC. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
---
+++ Names to Faces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Michael Harrison on 3/4/19.
+//  Copyright © 2019 Myco Labs LLC. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
